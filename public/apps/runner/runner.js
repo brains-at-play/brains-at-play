16a91464aae52deb458e62c65e2ece87a5456d3f
ahmedkadd <akadd8@gmail.com>
2021-02-20 13:37:26 -0800

    Updated floor size and camera position

--- public/apps/runner/runner.js
+++ public/apps/runner/runner.js
@@ -1,15 +1,16 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.y = 10;
-camera.position.z = 500;
+camera.position.z = 150;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const floorGeometry = new THREE.BoxGeometry(100, 1000, 1);
+const floorGeometry = new THREE.BoxGeometry(150, 1000, 1);
 const floorMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+floor.rotation.x = 2;
 scene.add( floor );
 
 function animate() {
