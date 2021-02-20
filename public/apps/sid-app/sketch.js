8ebe69a9aa776abd7cea0b574a0d9d639c58daf8
Siddhant Shenoy <siddhant8897@gmail.com>
2021-02-20 13:40:30 -0800

    Basic config to lead/save classifiers working now!

--- public/apps/sid-app/sketch.js
+++ public/apps/sid-app/sketch.js
@@ -1,13 +1,24 @@
 
+
   let connectToggle;
   let disconnectToggle;
   let museToggle;
 
   let margin = 100;
-  let colors = []
+  let colors = [];
 
   // Declare variables for training and testing data
   let feetTraining, rightTraining, feetTesting, rightTesting;
+
+  // Set classfier name and load/save toggle (load -> true, save -> false)
+  let classifierName = 'lda';
+  let loadOrSaveToggle = true;
+
+  // Toggle to use filter for bandpass
+  let useFilter = false;
+
+  // Declare variables for classifiers
+  let classifier, classifierPath;
 
   preload = () => {
     // Load training data
@@ -17,54 +28,19 @@
     // Load testing data
     feetTesting = loadTable('data/feet-testing.csv', 'csv');
     rightTesting = loadTable('data/righthand-testing.csv', 'csv');
+
+    // Load classifier if loadOrSaveToggle is true
+    if(loadOrSaveToggle) {
+      switch(classifierName) {
+        case 'lda': classifierPath = "classifiers/ldaParams.json"; break;
+      }
+      classifier = loadJSON(classifierPath);
+    }
   }
 
   setup = () => {
-    // Convert data array from string to numeric datatype
-    feetTraining = convertStrToNum(feetTraining.getArray());
-    rightTraining = convertStrToNum(rightTraining.getArray());
-    feetTesting = convertStrToNum(feetTesting.getArray());
-    rightTesting = convertStrToNum(rightTesting.getArray());
-
-    /* Logic for training classifier */
-    
-    // Project it with CSP
-    let cspParams = bci.cspLearn(feetTraining, rightTraining);
-
-    // Compute training data features
-    let featuresFeetTraining = computeFeatures(cspParams, feetTraining);
-    let featuresRightTraining = computeFeatures(cspParams, rightTraining);
-
-    // Learn an LDA classifier
-    let ldaParams = bci.ldaLearn(featuresFeetTraining, featuresRightTraining);
-
-    // Compute testing data features
-    let featuresFeetTesting = computeFeatures(cspParams, feetTesting);
-    let featuresRightTesting = computeFeatures(cspParams, rightTesting);
-
-    // Classify testing data
-    let classify = (feature) => {
-        let prediction = bci.ldaProject(ldaParams, feature);
-        return (prediction < 0) ? 0 : 1;
-    }
-    let feetPredictions = featuresFeetTesting.map(classify);
-    let rightPredictions = featuresRightTesting.map(classify);
-
-    // Evaluate the classifer
-    let feetActual = new Array(feetPredictions.length).fill(0);
-    let rightActual = new Array(rightPredictions.length).fill(1);
-
-    let predictions = feetPredictions.concat(rightPredictions);
-    let actual = feetActual.concat(rightActual);
-
-    let confusionMatrix = bci.confusionMatrix(predictions, actual);
-
-    let bac = bci.balancedAccuracy(confusionMatrix);
-
-    console.log('confusion matrix');
-    console.log(bci.toTable(confusionMatrix));
-    console.log('balanced accuracy');
-    console.log(bac);   
+    // LDA classifier example
+    trainAndTestClassifier();
 
     // Garrett's code
     for (let i = 0; i < 50; i++){
@@ -218,11 +194,30 @@
       });
     }
 
-    function computeFeatures(cspParams, eeg){
+    computeFeatures = (cspParams, eeg) => {
       let epochSize = 64; // About a fourth of a second per feature
       let trialLength = 750; // Each set of 750 samples is from a different trial
-  
-      let features = bci.windowApply(eeg, trial => {  
+
+      let features = bci.windowApply(eeg, trial => {
+
+          // Bandpass filter the trial if useFilter is true
+          if(useFilter) {
+            /* Set filter parameters if filter will be used */
+            // Bandpass between 7 and 30 Hz
+            let sampleRate = 250;
+            let lowFreq = 7;
+            let highFreq = 30;
+            let filterOrder = 128;
+            let firCalculator = new Fili.FirCoeffs();
+            let coeffs = firCalculator.bandpass({order: filterOrder, Fs: sampleRate, F1: lowFreq, F2: highFreq});
+            let filter = new Fili.FirFilter(coeffs);
+
+            // Apply filter
+            let channels = bci.transpose(trial);
+            channels = channels.map(signal => filter.simulate(signal).slice(filterOrder));
+            trial = bci.transpose(channels);
+          }
+        
           // Apply CSP over each 64 sample window with a 50% overlap between windows
           return bci.windowApply(trial, epoch => {
               // Project the data with CSP and select the 16 most relevant signals
@@ -234,4 +229,68 @@
   
       // Concat the features from each trial
       return [].concat(...features);
+    }
+
+    trainAndTestClassifier = () => {
+      /* Convert data array from string to numeric datatype */
+      feetTraining = convertStrToNum(feetTraining.getArray());
+      rightTraining = convertStrToNum(rightTraining.getArray());
+      feetTesting = convertStrToNum(feetTesting.getArray());
+      rightTesting = convertStrToNum(rightTesting.getArray());
+
+      /* Logic for training classifier */
+      
+      // Project data with CSP
+      let cspParams = bci.cspLearn(feetTraining, rightTraining);
+
+      // Compute training data features
+      let featuresFeetTraining = computeFeatures(cspParams, feetTraining);
+      let featuresRightTraining = computeFeatures(cspParams, rightTraining);
+
+      // Train and save the classifier if toggle is on save
+      if(!loadOrSaveToggle) {
+        switch(classifierName) {
+          case 'lda':
+            // Train LDA classifier
+            classifier = bci.ldaLearn(featuresFeetTraining, featuresRightTraining);
+
+            // Save LDA classifier
+            saveJSON(classifier, 'ldaParams.json');
+            break;
+        }
+      }
+
+      // Compute testing data features
+      let featuresFeetTesting = computeFeatures(cspParams, feetTesting);
+      let featuresRightTesting = computeFeatures(cspParams, rightTesting);
+
+      // Classify testing data
+      let classify = (feature) => {
+          let prediction;
+          switch(classifierName) {
+            case 'lda': prediction = bci.ldaProject(classifier, feature); break;
+          }
+          // Filter out values between -0.5 and 0.5 as unknown classes
+          if(prediction < -0.5) return 0;
+          if(prediction > 0.5) return 1;
+          return -1; 
+      }
+      let feetPredictions = featuresFeetTesting.map(classify).filter(value => value != -1);
+      let rightPredictions = featuresRightTesting.map(classify).filter(value => value != -1);
+
+      // Evaluate the classifer
+      let feetActual = new Array(feetPredictions.length).fill(0);
+      let rightActual = new Array(rightPredictions.length).fill(1);
+
+      let predictions = feetPredictions.concat(rightPredictions);
+      let actual = feetActual.concat(rightActual);
+
+      let confusionMatrix = bci.confusionMatrix(predictions, actual);
+
+      let bac = bci.balancedAccuracy(confusionMatrix);
+
+      console.log('confusion matrix');
+      console.log(bci.toTable(confusionMatrix));
+      console.log('balanced accuracy');
+      console.log(bac);
     }
