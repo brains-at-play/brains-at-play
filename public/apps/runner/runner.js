3d6070efe831811b79c94b883e0a8a5a59337979
ahmedkadd <akadd8@gmail.com>
2021-02-20 13:29:41 -0800

    Update camera position

--- public/apps/runner/runner.js
+++ public/apps/runner/runner.js
@@ -1,16 +1,16 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.y = 10;
+camera.position.z = 500;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const floorGeometry = new THREE.BoxGeometry();
+const floorGeometry = new THREE.BoxGeometry(100, 1000, 1);
 const floorMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
 scene.add( floor );
-
-camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
