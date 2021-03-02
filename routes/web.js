8977251b98856c421a639a7eb3e0a78dbd19e424
Garrett <gflynn@usc.edu>
2021-03-02 14:13:42 -0600

    Generate URL correctly

--- routes/web.js
+++ routes/web.js
@@ -8,7 +8,7 @@ let appsDir = {}
 fs.readdir(path.join('public','apps'), (err, files) => {
   files = files.filter(file => !file.includes('.'))
   files.forEach(file => {
-    let dir = path.join('public', 'apps',file)
+    let dir = 'public/apps/' + file
     let info = fs.readFileSync(path.join('public', 'apps',file,'info.json'));
     appsDir[file] = JSON.parse(info)
     appsDir[file].path = dir
